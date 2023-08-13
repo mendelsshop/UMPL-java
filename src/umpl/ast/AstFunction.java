1a999e3ff59a9fb8cf9d7fbe898b62da8a19b5f9
mendelsshop <mendelsshop@gmail.com>
2023-08-13 01:38:02 -0400

    moved to using jars stupid javar

--- src/umpl/ast/AstFunction.java
+++ src/umpl/ast/AstFunction.java
@@ -1,5 +1,35 @@
 package umpl.ast;
 
-public class AstFunction {
-    
+import java.util.List;
+import java.util.Optional;
+
+import com.vdurmont.emoji.EmojiManager;
+
+import parser_combinator.Parser;
+
+public class AstFunction extends Ast {
+    Optional<String> name;
+    int paramCount;
+    Optional<Variadic> params;
+    List<Ast> scope;
+
+    public AstFunction(Optional<String> name, int paramCount, Optional<Variadic> params, List<Ast> scope) {
+        this.name = name;
+        this.paramCount = paramCount;
+        this.params = params;
+        this.scope = scope;
+    }
+
+    public static final Parser<Ast> parse = null;
+
+    public enum Variadic {
+        AtLeast1,
+        AtLeast0,
+    }
+
+    @Override
+    public String toString() {
+        return "AstFunction [name=" + name + ", paramCount=" + paramCount + ", params=" + params + ", scope=" + scope
+                + "]";
+    }
 }
