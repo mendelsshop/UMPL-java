d96696ec0c72ef2d908727891cbc2e13ce9f1abc
mendelsshop <mendelsshop@gmail.com>
2023-08-21 09:11:26 -0400

    add analyzing for labels

--- src/umpl/analyzer/Anaylzer.java
+++ src/umpl/analyzer/Anaylzer.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Anaylzer {
     HashMap<String, List<String>> links;
 
+    public HashMap<String, List<String>> getLinks() {
+        return links;
+    }
+
     public void pushLink(String link, List<String> places) {
         links.put(link, places);
     }
--- src/umpl/ast/AstLabel.java
+++ src/umpl/ast/AstLabel.java
@@ -1,5 +1,7 @@
 package umpl.ast;
 
+import java.util.Optional;
+
 import misc.Result.Result;
 import parser_combinator.Parser;
 import parser_combinator.Parsers;
@@ -12,9 +14,20 @@ public class AstLabel extends Ast {
 
     public AstLabel(String val) {
         this.val = val;
+        type = Type.Normal;
+    }
+
+    enum Type {
+        Normal, ComeTo
     }
 
     String val;
+    Type type;
+
+    private AstLabel(String val, Type type) {
+        this.val = val;
+        this.type = type;
+    }
 
     public static final Parser<Ast> parser = lableParser
             .Map(c -> new AstLabel(c));
@@ -37,8 +50,23 @@ public class AstLabel extends Ast {
 
     @Override
     public Ast analyze_labels(Anaylzer analyzer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyze_labels'");
+        // TODO: this might not be needed as we can just figure out what the label does
+        // at eval time
+        Optional<Optional<Ast>> replace = analyzer.getLinks().entrySet().stream().<Optional<Ast>>map(c -> {
+            if (c.getKey() == val) {
+                return Optional.of(new AstLabel(val, Type.ComeTo));
+            } else if (c.getValue().contains(val)) {
+                return Optional.ofNullable(new AstLabel(c.getKey()));
+            } else {
+                return Optional.empty();
+
+            }
+        }).findAny();
+        if (replace.isPresent()) {
+            return replace.get().get();
+        } else {
+            return this;
+        }
     }
 
 }
